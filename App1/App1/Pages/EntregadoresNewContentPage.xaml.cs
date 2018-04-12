85ff60a42f1f54b35f838d90a1b74f62ff21ae97
Júnior Silva <junior_dariana@hotmail.com>
2018-04-11 22:08:32 -0300

    Add Page of Insertion of news entregadores

--- App1/App1/Pages/EntregadoresNewContentPage.xaml.cs
+++ App1/App1/Pages/EntregadoresNewContentPage.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using App1.DAL;
+using App1.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -12,9 +14,39 @@ namespace App1.Pages
 	[XamlCompilation(XamlCompilationOptions.Compile)]
 	public partial class EntregadoresNewContentPage : ContentPage
 	{
+        private EntregadorDAL dalEntregador = EntregadorDAL.GetInstance();
+        
+
 		public EntregadoresNewContentPage ()
 		{
 			InitializeComponent ();
+            PreparaParaNovoEntregador();
 		}
-	}
+
+        public void BtnGravarClick(object sender, EventArgs e)
+        {
+            if(nome.Text.Trim() == string.Empty || telefone.Text== string.Empty)
+            {
+                this.DisplayAlert("Erro", "Você precisa informar o nome e telefone para o entregador.", "OK");
+            }
+            else
+            {
+                dalEntregador.Add(new Entregador()
+                {
+                    Id = Convert.ToUInt32(identregador.Text),
+                    Nome = nome.Text,
+                    Telefone = telefone.Text
+                });
+                PreparaParaNovoEntregador();
+            }
+        }
+
+        private void PreparaParaNovoEntregador()
+        {
+            var novoId = dalEntregador.GetAll().Max(x => x.Id) + 1;
+            identregador.Text = novoId.ToString().Trim();
+            nome.Text = string.Empty;
+            telefone.Text = string.Empty;
+        }
+    }
 }
