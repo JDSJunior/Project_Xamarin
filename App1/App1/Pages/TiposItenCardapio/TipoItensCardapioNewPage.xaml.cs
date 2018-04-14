0996859677296faae52c7b0a34159d8dccab93ba
Júnior Silva <junior_dariana@hotmail.com>
2018-04-13 22:12:49 -0300

    Add insercion page of images

--- App1/App1/Pages/TiposItenCardapio/TipoItensCardapioNewPage.xaml.cs
+++ App1/App1/Pages/TiposItenCardapio/TipoItensCardapioNewPage.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using App1.DAL;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -12,9 +13,26 @@ namespace App1.Pages.TiposItenCardapio
 	[XamlCompilation(XamlCompilationOptions.Compile)]
 	public partial class TipoItensCardapioNewPage : ContentPage
 	{
+        private TipoItemCardapioDAL dalTiposItenscardapio = TipoItemCardapioDAL.GetInstance();
+        private string caminhoArquivo;
+
 		public TipoItensCardapioNewPage ()
 		{
 			InitializeComponent ();
+            PreparaParaNovoTipoItemCardapio();
+            RegistraClickBotaoCamera(idtipoitemcardapio.Text.Trim());
+            RegistraClickBotaoAlbum();
 		}
-	}
+
+        private void PreparaParaNovoTipoItemCardapio()
+        {
+            var novoId = dalTiposItenscardapio.GetAll().Max(x => x.Id) + 1;
+
+            idtipoitemcardapio.Text = novoId.ToString().Trim();
+            nome.Text = string.Empty;
+            fotoitemcardapio.Source = null;
+        };
+
+
+    }
 }
